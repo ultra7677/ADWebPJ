aaa3fa74d8004dd273eb0b0b1ce15a3cbf5757ae
ultra7677 <ultra7677@gmail.com>
2016-06-28 19:04:27 +0800

    MVC with Ajax

--- complete/src/main/java/com/adweb/app/controller/SampleController.java
+++ complete/src/main/java/com/adweb/app/controller/SampleController.java
@@ -1,18 +1,32 @@
 package com.adweb.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.adweb.app.model.LoginForm;
 
 
 @Controller 
-public class SampleController {
+public class SampleController 
+{
 	
 	 
-	 @RequestMapping(value = "/")
-	 public String loadLoginPage() {
+	 @RequestMapping(value = "/login.html")
+	 public String loadLoginPage() 
+	 {
 		   	return "templates/index.html";
 	 }
 	 
 	 
+	 @RequestMapping(value = "/login")
+	 public @ResponseBody Boolean loginRequest(@RequestBody LoginForm loginForm)
+	 {
+		System.out.println(loginForm.getUsername());
+		System.out.println(loginForm.getPassword());
+		// handle method
+		return true;
+	 }
 		
 }
+	
